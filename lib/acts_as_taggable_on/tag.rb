4a9c72c25a811d25f7c5788fa5b98aa2e1afcfd6
Jeremy Grant <jeremy@40people.com.au>
2009-03-08 16:17:05 +1100

    added check_for functionality to Tag model to allow for a clean way to only delete a tag if it has no taggings accociated to it.

--- lib/acts_as_taggable_on/tag.rb
+++ lib/acts_as_taggable_on/tag.rb
@@ -3,8 +3,10 @@ class Tag < ActiveRecord::Base
   
   validates_presence_of :name
   validates_uniqueness_of :name
-  
-  # LIKE is used for cross-database case-insensitivity
+
+  before_destroy :stop_destroy_if_check_for_contains_taggings
+
+  # case-insensitive like used
   def self.find_or_create_with_like_by_name(name)
     find(:first, :conditions => ["name LIKE ?", name]) || create(:name => name)
   end
@@ -19,5 +21,26 @@ class Tag < ActiveRecord::Base
   
   def count
     read_attribute(:count).to_i
+  end
+  
+  @@check_for_option_keys = [:context, :contexts, :taggable_type, :taggable_types]
+  
+  def check_for(options = {})
+    @check_for_options = options.reject{ |key, value| ! @@check_for_option_keys.include?(key) }
+    @check_for = true
+    self
+  end
+
+private
+  def stop_destroy_if_check_for_contains_taggings
+    taggings = self.taggings
+    @check_for_options.each_pair do |key, value| 
+      taggings = taggings.send("for_#{key}", value) if @@check_for_option_keys.include? key
+    end
+    check_for_taggings_count = taggings.count
+    if @check_for && check_for_taggings_count > 0
+      errors.add(:taggings, "Failed to delete tag #{self.name}. You need to remove the items associated to it before you can delete it")
+      false
+    end
   end
 end
--- lib/acts_as_taggable_on/tagging.rb
+++ lib/acts_as_taggable_on/tagging.rb
@@ -3,4 +3,20 @@ class Tagging < ActiveRecord::Base #:nodoc:
   belongs_to :taggable, :polymorphic => true
   belongs_to :tagger, :polymorphic => true
   validates_presence_of :context
+
+  named_scope :for_contexts, lambda{ |contexts|
+    {:conditions => ["context in (?)", context]}
+  }
+
+  named_scope :for_context, lambda{ |context|
+    {:conditions => ["context = ?", context]}
+  }
+
+  named_scope :for_taggable_types, lambda{ |taggable_types|
+    {:conditions => ["taggable_type in (?)", taggable_types.collect { |taggable_type| taggable_type.to_s }]}
+  }
+
+  named_scope :for_taggable_type, lambda{ |taggable_type|
+    {:conditions => ["taggable_type = ?", taggable_type.to_s]}
+  }
 end
